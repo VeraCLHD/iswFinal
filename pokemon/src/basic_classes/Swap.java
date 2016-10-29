1cd2d728391b299414a89aee0697d5c3181d6c7a
VeraGeorg <Vera@Coli-2015.localdomain>
2016-10-29 18:28:55 +0200

    each pokemon has a list of swapps: added. Also added a few prints for Testat

--- pokemon/src/basic_classes/Swap.java
+++ pokemon/src/basic_classes/Swap.java
@@ -30,14 +30,18 @@ public class Swap {
 						trainer1.removePokemon(pokemon1);
 						trainer2.addPokemons(pokemon1);
 						swapDate = new Date(System.currentTimeMillis());
+						pokemon1.getSwaps().add(this);
+						pokemon2.getSwaps().add(this);
 						
 					} else {
 						throw new Exception("Pokemon " + pokemon1.getName() + " kann nicht mit " + pokemon2.getName() + " getauscht werden, da beide vom Typ " + pokemon1.getType() + " sind.");
 					}
 				} else if(!pokemon1.isSwapAllow()){
+					
 					throw new Exception("Pokemon " + pokemon1.getName() + " ist nicht zum Tauschen freigegeben.");
 				} else if(!pokemon2.isSwapAllow()){
-					throw new Exception("Pokemon 2 is not allowed to swap.");
+					System.out.println(pokemon2.isSwapAllow());
+					throw new Exception("Pokemon " + pokemon2.getName() + " ist nicht zum Tauschen freigegeben.");
 				} else{
 					throw new Exception("Pokemon 1 and Pokemon 2 are not allowed to swap.");
 				}
@@ -60,7 +64,6 @@ public class Swap {
 		Pokemon pokemon1 = new Pokemon("Sith", Type.Fire);
 		Pokemon pokemon2 = new Pokemon("Curt", Type.Water);
 		Pokemon pokemon3 = new Pokemon("John", Type.Poison);
-		Pokemon pokemon4 = new Pokemon("Jimmy", Type.Fire);
 		
 		Trainer trainer1 = new Trainer("Jorn","L.");
 		Trainer trainer2 = new Trainer("Jorn","M.");
@@ -72,6 +75,7 @@ public class Swap {
 		Swap test = new Swap(trainer1, trainer2);
 		
 		// should work fine
+		System.out.println("This should work fine: pokemon 1 and pokemon 2 are being swaped.");
 		try{
 			test.execute( pokemon1, pokemon2);
 		} catch(Exception e){
@@ -80,6 +84,7 @@ public class Swap {
 		}
 		
 		// should print an error
+		System.out.println("This should print an error: pokemon 1 is not allowed to swap.");
 		try{
 			pokemon1.setSwapAllow(false);
 			test.execute( pokemon1, pokemon2);
@@ -87,6 +92,7 @@ public class Swap {
 			System.out.println(e);
 		}
 		
+		System.out.println("This should print an error: pokemon 1 is allowed to swap but pokemon 2 is set to the same type as pokemon 1.");
 		try{
 			pokemon1.setSwapAllow(true);
 			pokemon2.setType(Type.Fire);
